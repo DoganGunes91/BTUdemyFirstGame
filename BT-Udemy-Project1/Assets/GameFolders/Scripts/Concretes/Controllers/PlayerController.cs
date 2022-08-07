2c8ae4e147593096f33c364e6b47c7a628c3a1f3
DoganGunes91 <99689185+DoganGunes91@users.noreply.github.com>
2022-08-07 19:16:17 +0300

    Player Movement
    Hareketler ve döndürme tamamlandı

--- BT-Udemy-Project1/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ BT-Udemy-Project1/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -2,21 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BTUdemyProject1.Inputs;
+using BTUdemyProject1.Movements;
 
 namespace BTUdemyProject1.Controllers
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] float _force;
-        Rigidbody _rigidbody;
+        [SerializeField] float _turnSpeed = 100f;
+        [SerializeField] float _force = 1000f;
+
         DefaultInput _input;
+        Mover _mover;
+        Rotator _rotator;
 
         bool _isForceUp;
+        float _leftRight;
+
+        public float TurnSpeed => _turnSpeed;
+        public float Force => _force;
 
         private void Awake()
         {
-            _rigidbody = GetComponent<Rigidbody>();
             _input = new DefaultInput();
+            _mover = new Mover(this);
+            _rotator = new Rotator(this);
         }
 
         private void Update()
@@ -29,14 +38,18 @@ namespace BTUdemyProject1.Controllers
             {
                 _isForceUp = false;
             }
+
+            _leftRight = _input.LeftRight;
         }
 
         private void FixedUpdate()
         {
             if (_isForceUp)
             {
-                _rigidbody.AddForce(Vector3.up * _force * Time.deltaTime);
+                _mover.FixedTick();
             }
+
+            _rotator.FixedTick(_leftRight);
         }
     }
 }
--- BT-Udemy-Project1/Assets/GameFolders/Scripts/Concretes/Inputs/DefaultAction.cs
+++ BT-Udemy-Project1/Assets/GameFolders/Scripts/Concretes/Inputs/DefaultAction.cs
@@ -37,6 +37,15 @@ namespace BTUdemyProject1.Inputs
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""LeftRight"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""62462a78-aa0e-484e-98ec-a0c431464c2f"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press(behavior=2)"",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -50,6 +59,39 @@ namespace BTUdemyProject1.Inputs
                     ""action"": ""ForceUp"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""AD"",
+                    ""id"": ""0cd6705a-b2f5-4be4-b1f6-36739756ba78"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""LeftRight"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""7a0cf796-c560-4630-8465-e9470f135f5d"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""LeftRight"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""9e0d6a85-cf9a-48bb-a858-0ce26e9a8233"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""LeftRight"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
                 }
             ]
         }
@@ -59,6 +101,7 @@ namespace BTUdemyProject1.Inputs
             // Rocket
             m_Rocket = asset.FindActionMap("Rocket", throwIfNotFound: true);
             m_Rocket_ForceUp = m_Rocket.FindAction("ForceUp", throwIfNotFound: true);
+            m_Rocket_LeftRight = m_Rocket.FindAction("LeftRight", throwIfNotFound: true);
         }
 
         public void Dispose()
@@ -119,11 +162,13 @@ namespace BTUdemyProject1.Inputs
         private readonly InputActionMap m_Rocket;
         private IRocketActions m_RocketActionsCallbackInterface;
         private readonly InputAction m_Rocket_ForceUp;
+        private readonly InputAction m_Rocket_LeftRight;
         public struct RocketActions
         {
             private @DefaultAction m_Wrapper;
             public RocketActions(@DefaultAction wrapper) { m_Wrapper = wrapper; }
             public InputAction @ForceUp => m_Wrapper.m_Rocket_ForceUp;
+            public InputAction @LeftRight => m_Wrapper.m_Rocket_LeftRight;
             public InputActionMap Get() { return m_Wrapper.m_Rocket; }
             public void Enable() { Get().Enable(); }
             public void Disable() { Get().Disable(); }
@@ -136,6 +181,9 @@ namespace BTUdemyProject1.Inputs
                     @ForceUp.started -= m_Wrapper.m_RocketActionsCallbackInterface.OnForceUp;
                     @ForceUp.performed -= m_Wrapper.m_RocketActionsCallbackInterface.OnForceUp;
                     @ForceUp.canceled -= m_Wrapper.m_RocketActionsCallbackInterface.OnForceUp;
+                    @LeftRight.started -= m_Wrapper.m_RocketActionsCallbackInterface.OnLeftRight;
+                    @LeftRight.performed -= m_Wrapper.m_RocketActionsCallbackInterface.OnLeftRight;
+                    @LeftRight.canceled -= m_Wrapper.m_RocketActionsCallbackInterface.OnLeftRight;
                 }
                 m_Wrapper.m_RocketActionsCallbackInterface = instance;
                 if (instance != null)
@@ -143,6 +191,9 @@ namespace BTUdemyProject1.Inputs
                     @ForceUp.started += instance.OnForceUp;
                     @ForceUp.performed += instance.OnForceUp;
                     @ForceUp.canceled += instance.OnForceUp;
+                    @LeftRight.started += instance.OnLeftRight;
+                    @LeftRight.performed += instance.OnLeftRight;
+                    @LeftRight.canceled += instance.OnLeftRight;
                 }
             }
         }
@@ -150,6 +201,7 @@ namespace BTUdemyProject1.Inputs
         public interface IRocketActions
         {
             void OnForceUp(InputAction.CallbackContext context);
+            void OnLeftRight(InputAction.CallbackContext context);
         }
     }
 }
