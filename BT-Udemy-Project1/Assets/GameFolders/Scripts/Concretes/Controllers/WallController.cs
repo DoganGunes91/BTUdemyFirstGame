ae9812af1f87f2d1cfc39064cf58d386d2e8ca35
DoganGunes91 <99689185+DoganGunes91@users.noreply.github.com>
2022-08-13 19:41:56 +0300

    Start Floor
    Başlangıç ayarları yapıldı

---
+++ BT-Udemy-Project1/Assets/GameFolders/Scripts/Concretes/Controllers/WallController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BTUdemyProject1.Controllers
+{
+    public class WallController : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision other)
+        {
+            PlayerController player = other.collider.GetComponent<PlayerController>();
+
+            if (player != null)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
